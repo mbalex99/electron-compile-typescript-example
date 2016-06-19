d31f662c5d99a3bf3e63732fb950d7ce22fab34d
Maximilian Alexander <max@zinkpulse.com>
2016-06-18 17:15:48 -0700

    removing ignored files

--- node_modules/electron-compile/src/es6-shim.js
+++
@@ -1,6 +0,0 @@
-var path = require('path');
-var electronCompile = require('electron-compile');
-
-var packageJson = require('./package.json');
-let initScript = path.resolve(__dirname, packageJson.originalMain);
-electronCompile.init(__dirname, initScript);
--- node_modules/electron-compile/test/electron-app/src/main.coffee
+++
@@ -1,5 +0,0 @@
-$ ->
-  form = new EncodeFormView
-    logsElem: $("div.logs")
-
-  form.render().$el.appendTo $("div.form")
